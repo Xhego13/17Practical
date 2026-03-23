a81b7a1f3a88992e67183698e446f54bf992342d
Mzawupheli Xhego <4428828@myuwc.ac.za>
2026-03-23 15:10:18 +0200

    Update tryBst.java

--- tryBst.java
+++ tryBst.java
@@ -1,5 +1,25 @@
 public class tryBst{
- 
+  public void insert(tNode n) {
+    if (root == null) {
+      root = n;
+      root.setParent(null);
+    }else
+      root.insert(n);
+    }
+  public void insert(tNode n){
+    if (n.key < key){ 
+      if (left == null){
+        n.parent = this;
+        left = n;
+      }else{
+        left.insert(n);}
+    }else{ 
+      if (right == null) {
+         n.parent = this;
+         right = n;
+      }else{
+        right.insert(n);}
+    } 
 }
 public tNode() {
   key = 0;
@@ -25,27 +45,6 @@ public tNode() {
   public tNode getParent() {return parent; }
   public void setParent(tNode n) {parent = n;}
 }
-public void insert(tNode n) {
-  if (root == null) {
-    root = n;
-    root.setParent(null);
-  }else
-    root.insert(n);
-  }
-public void insert(tNode n){
-if (n.key < key){ 
-  if (left == null){
-    n.parent = this;
-    left = n;
-  }else{
-    left.insert(n);}
-}else{ 
-  if (right == null) {
-     n.parent = this;
-     right = n;
-  }else{
-    right.insert(n);}
-}
 
 
 
@@ -61,12 +60,6 @@ if (n.key < key){
 
 
 
-}
-void buildBalancedBST(int start, int end) {
-    if (start > end) return;
-    int mid = (start + end) / 2;
-    insert(mid);
-    buildBalancedBST(start, mid - 1);
-    buildBalancedBST(mid + 1, end);
-}  
+
+
 
