a2ff7fc7d15258c7b9410bddc32c1129fac2451d
Mzawupheli Xhego <4428828@myuwc.ac.za>
2026-03-23 14:29:12 +0200

    tnode constructor implementation

--- tryBst.java
+++ tryBst.java
@@ -6,6 +6,13 @@ public class tryBst{
         key = item;
         left = right = null
     }
+  }
+  void buildBalancedBST(int start, int end) {
+    if (start > end) return;
+    int mid = (start + end) / 2;
+    insert(mid);
+    buildBalancedBST(start, mid - 1);
+    buildBalancedBST(mid + 1, end);
   }  
 }
 
