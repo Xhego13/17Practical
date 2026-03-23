0d6283a4b44296ba9660b5e6e3b34be5669c842c
Mzawupheli Xhego <4428828@myuwc.ac.za>
2026-03-23 14:41:14 +0200

    Update tryBst.java

--- tryBst.java
+++ tryBst.java
@@ -1,18 +1,19 @@
 public class tryBst{
-  class tNode {
-    int key;
-    tNode left, right;
-    tNode(int item) {
-        key = item;
-        left = right = null
-    }
-  }
-  void buildBalancedBST(int start, int end) {
+ 
+}
+class tNode {
+      int key;
+      tNode left, right;
+      tNode(int item) {
+      key = item;
+      left = right = null
+      }
+}
+void buildBalancedBST(int start, int end) {
     if (start > end) return;
     int mid = (start + end) / 2;
     insert(mid);
     buildBalancedBST(start, mid - 1);
     buildBalancedBST(mid + 1, end);
-  }  
-}
+}  
 
